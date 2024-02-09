57bb2e6473ed6353839ae035f305d864f8e8a69f
Marcos Paulo <marcos.poo@gmail.com>
2024-02-08 23:25:37 -0300

    Aula 3 - Consultando e paginando.
    - Como recuperar informações da API através da criação de actions.
    - O verbo GET visa retornar recursos da API.
    - Como enviar parâmetros através da URL de requisição.
    - Como filtrar recursos para retornar para o usuário utilizando LINQ.
    - Status 404 indica que um recurso não foi encontrado.
    - Como tornar nossos retornos mais enxutos através de conceitos de paginação.
    - Como aplicar paginação através dos métodos Skip() e Take().

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -4,6 +4,7 @@ namespace FilmesAPI.Models;
 
 public class Filme
 {
+    public int Id { get; set; }
     [Required(ErrorMessage = "O título do filme é obrigatório")]
     [MaxLength(100, ErrorMessage = "O tamanho do gênero não pode exceder 100 caracteres")]
     public string Titulo { get; set; }
@@ -15,6 +16,4 @@ public class Filme
     [Required(ErrorMessage = "A duração do filme é obrigatório")]
     [Range(70, 600, ErrorMessage = "A duração deve ter entre 1 e 360 minutos")]
     public int Duracao { get; set; }
-
-    public string Diretor { get; set; }
 }
