2a71b977138841ca1bf56db03aa818b862bd48a4
Marcos Paulo <marcos.poo@gmail.com>
2024-02-10 14:53:27 -0300

    Aula 5 - Atualizando e removendo.
    - O verbo PUT pode ser utilizado quando queremos atualizar algum recurso no sistema.
    - O verbo PATCH também pode ser utilizado para atualizações, porém de maneira parcial.
    - Apesar do verbo PATCH não exigir a passagem completa do objeto a ser atualizado, exige uma configuração mais complexa.
    - O verbo DELETE deve ser utilizado quando queremos deletar um recurso no sistema.
    - Quando um recurso é atualizado ou removido com sucesso, devemos retornar status 204 (No Content).

--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -2,17 +2,15 @@ using FilmesAPI.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
 var connectionString = builder.Configuration.GetConnectionString("FilmeConnection");
 
-builder.Services.AddDbContext<FilmeContext>(opts =>
-opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+builder.Services.AddDbContext<FilmeContext>(opts => opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
